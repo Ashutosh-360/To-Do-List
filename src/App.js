11037d62cd69633a73f6ea0c5603fa80ae4a5849
Ashutosh-360 <ashutoshnaik360@gmail.com>
2022-10-10 11:34:54 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,99 @@
-import logo from './logo.svg';
+
+import { useState,useEffect } from 'react';
 import './App.css';
+import moon from "./images/icon-moon.svg";
+import sun from "./images/icon-sun.svg"
+import lightBackground from "./images/bg-desktop-light.jpg";
+import darkBackground from "./images/bg-desktop-dark.jpg";
 
 function App() {
+
+  const getLocalListItem=()=>{
+    let toDoList=localStorage.getItem("toDoList");
+    if(toDoList)
+    {
+      return JSON.parse(localStorage.getItem("toDoList"));
+    }
+    else
+    {
+      return [];
+    }
+  }
+
+  const [item,setItem]=useState("");
+  const [listItem,setListItem]=useState(getLocalListItem());
+  const [darkMode,setDarkMode]=useState(false);
+
+
+
+  
+
+  const itemInputFunc=(event)=>
+  {
+  
+      setItem(event.target.value);
+    
+  }
+  const addItemFunc=(e)=>
+  {
+    if(e.key==="Enter" && item!=="")
+    {
+      setListItem([...listItem,item]);
+      setItem("");
+    }
+    
+  }
+  const completedFunc=(event)=>
+  {
+      event.target.parentElement.parentElement.firstChild.style.textDecoration="line-through";
+      event.target.parentElement.parentElement.firstChild.style.color="green";
+    
+  }
+  const deleteFunc=(event)=>
+  {
+    event.target.parentNode.parentNode.remove();
+  }
+  const toggleModeHandler=()=>{
+      setDarkMode(!darkMode);
+  }
+  useEffect(() => {
+    localStorage.setItem("toDoList",JSON.stringify(listItem));
+    
+  }, [listItem,deleteFunc]);
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+     <div className='input-container' style={darkMode?{backgroundImage:`url(${darkBackground})`}:{backgroundImage:`url(${lightBackground})`}}>
+     <div className='input-content'>
+      <div className='input-heading'>
+
+    <h1>TODO</h1>
+    <img onClick={toggleModeHandler} src={darkMode?sun:moon} alt="" />
+      </div>
+        <input onChange={itemInputFunc} onKeyPress={addItemFunc} value={item} type="text" placeholder='Create a new todo...' style={darkMode?{background:"hsl(235, 24%, 19%)",color:"hsl(234, 39%, 85%)"}:{background:"white"}}/>
+     </div>
+     </div>
+     <div className='list-container' style={darkMode?{background:"black"}:{background:"white"}}>
+     <div className='list-content'>
+        <ul>
+       
+          {listItem.map((listElement,index)=>{
+            return <>
+            
+            <li key={index} style={darkMode?{background:"hsl(235, 24%, 19%)",color:"hsl(234, 39%, 85%)"}:{background:"white"}}>
+            <h3>{listElement}</h3>
+            <div className='button-container'>
+            <button onClick={completedFunc}>Complete</button>
+            <button className='delete' onClick={deleteFunc}>Delete</button>
+            </div>
+            </li>
+            </>
+          })}
+        </ul>
+     </div>
+     </div>
+     </>
   );
 }
 
